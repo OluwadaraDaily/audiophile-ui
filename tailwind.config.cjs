234a53100e4e7156518e3716d06fc5f458717bed
Oluwadara Oloye <daraoloye99@gmail.com>
2024-09-19 00:53:17 +0100

    style: setup tailwind color pallete

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,15 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#D87D4A',
+        'primary-light': '#FBAF85',
+        'black-light': '#101010',
+        'grey': '#F1F1F1',
+        'grey-light': '#FAFAFA',
+      }
+    },
   },
   variants: {
     extend: {},
