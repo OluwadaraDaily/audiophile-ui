50d645211a611c62d359ea0170f5055b89c70f4e
Oluwadara Oloye <daraoloye99@gmail.com>
2024-09-23 23:46:02 +0100

    fix: tailwind cofiguration

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    This is the Home View  
+  <div class="m-5">
+    <div class="w-full p-4 bg-blue-300 rounded-xl">
+      This is the Home View.  
+    </div>
   </div>
 </template>
 
