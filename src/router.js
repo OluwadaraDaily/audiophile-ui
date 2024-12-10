40f21f18f4ab000953ebf04a005cee4c174d6dab
Oluwadara Oloye <daraoloye99@gmail.com>
2024-12-10 23:34:33 +0100

    chore: update routing implementation to use HTML5 history mode

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createMemoryHistory, createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from './views/HomeView/HomeView.vue'
 import HeadphonesView from './views/HeadphonesView/HeadphonesView.vue'
@@ -7,13 +7,29 @@ import EarphonesView from "./views/EarphonesView/EarphonesView.vue"
 
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/headphones', component: HeadphonesView },
-  { path: '/speakers', component: SpeakersView },
-  { path: '/earphones', component: EarphonesView },
+  {
+    path: '/',
+    name: "home",
+    component: HomeView
+  },
+  {
+    path: '/headphones',
+    name: "headphones",
+    component: HeadphonesView
+  },
+  {
+    path: '/speakers',
+    name: "speakers",
+    component: SpeakersView
+  },
+  {
+    path: '/earphones',
+    name: "earphones",
+    component: EarphonesView
+  },
 ]
 
 export const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
