9796ac34c9bc4c781f10deb1d8c5158053899992
Oluwadara Oloye <daraoloye99@gmail.com>
2025-01-17 20:36:58 +0100

    feature: implement payment details section

--- src/views/CheckoutView/CheckoutView.vue
+++ src/views/CheckoutView/CheckoutView.vue
@@ -5,16 +5,19 @@
         <router-link to="/" class="border-none outline-none text-black opacity-50 hover:text-primary">Go Back</router-link>
       </div>
       <div class="flex flex-col lg:flex-row gap-6">
-        <div class="lg:basis-[75%] lg:h-[100vh] border border-black p-6 rounded-md">
+        <div class="lg:basis-[75%] lg:h-[100vh] py-6 w-[90%] mx-auto rounded-md">
           <h2 class="h2 uppercase mb-4">checkout</h2>
           <div class="mb-8">
             <billing-details/>
           </div>
-          <div>
+          <div class="mb-6">
             <shipping-info/>
           </div>
+          <div>
+            <payment-details/>
+          </div>
         </div>
-        <div class="lg:basis-[25%] lg:h-[100vh] border border-blue-400 p-6 rounded-md">
+        <div class="lg:basis-[25%] lg:h-[100vh] border border-blue-400 py-6 w-[90%] mx-auto rounded-md">
 
         </div>
       </div>
@@ -27,6 +30,7 @@
 // Imports
 import BillingDetails from "./BillingDetails.vue"
 import ShippingInfo from "./ShippingInfo.vue"
+import PaymentDetails from "./PaymentDetails.vue";
 
 </script>
 
