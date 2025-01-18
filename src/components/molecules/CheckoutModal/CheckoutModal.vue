d675241c325a2c936245bd6a82cc3f12f78981f6
Oluwadara Oloye <daraoloye99@gmail.com>
2025-01-18 15:05:36 +0100

    style: make checkout modal responsive

--- src/components/molecules/CheckoutModal/CheckoutModal.vue
+++ src/components/molecules/CheckoutModal/CheckoutModal.vue
@@ -2,15 +2,15 @@
   <div 
     class="bg-black bg-opacity-60 w-full z-10 h-full flex justify-center"
   >
-    <div class="bg-white py-10 rounded-md w-[90%] mx-auto z-40 h-max mt-10">
+    <div class="bg-white py-10 rounded-md w-[90%] md:w-[70%] lg:w-[540px] mx-auto z-40 h-max mt-10 md:mt-[100px]">
       <div class="w-[90%] mx-auto">
         <div class="h-[64px] w-[64px] bg-primary flex items-center justify-center rounded-[50%] mb-6">
           <checkmark-svg/>
         </div>
         <h5 class="h5 uppercase text-wrap-balance mb-4">thank you for your order</h5>
         <p class="p opacity-50 mb-4">You will receive an email confirmation shortly.</p>
-        <div>
-          <div class="bg-grey py-4 rounded-tl-xl rounded-tr-xl">
+        <div class="md:flex md:w-full">
+          <div class="bg-grey py-4 rounded-tl-xl rounded-tr-xl md:rounded-none md:rounded-tl-xl md:rounded-bl-xl md:w-full">
             <div class="w-[90%] mx-auto">
               <cart-item
                 v-bind="cartItem"
@@ -27,7 +27,7 @@
               </div>
             </div>
           </div>
-          <div class="bg-black py-4 rounded-bl-xl rounded-br-xl">
+          <div class="bg-black py-4 rounded-bl-xl rounded-br-xl md:rounded-none md:rounded-tr-xl md:rounded-br-xl md:w-full md:flex md:items-center">
             <div class="w-[90%] mx-auto text-white">
               <p class="p text-white opacity-50 uppercase mb-2">grand total</p>
               <p class="h6 text-white">{{ formatCurrency(cartStore.getGrandTotal) }}</p>
