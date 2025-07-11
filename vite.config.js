c16600248259e2f6e157fe765cff2988e0d00312
Oluwadara Oloye <daraoloye99@gmail.com>
2025-07-11 10:33:37 +0100

    feat: add vite-plugin-compression for Brotli and Gzip support in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import compression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx()
+    vueJsx(),
+    compression({
+      algorithm: 'brotliCompress',
+      include: [/\.(js|mjs|json|css|html)$/],
+      disable: false,
+      threshold: 10240,
+    }),
+    compression({
+      algorithm: 'gzip',
+      include: [/\.(js|mjs|json|css|html)$/],
+      disable: false,
+      threshold: 10240,
+    })
   ],
   resolve: {
     alias: {
